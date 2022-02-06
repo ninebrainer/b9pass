72f12b263ce71e427d2a9d9529de5ae5f8dff4d0
ninebrainer <97753206+ninebrainer@users.noreply.github.com>
2022-02-06 15:53:21 +0100

    Update b9pass.sh

--- b9pass.sh
+++ b9pass.sh
@@ -40,7 +40,7 @@ RED="\033[0;31m" 		# Error / Issues
 GREEN="\033[0;32m"		# Successful       
 BOLD="\033[01;01m"    	# Highlight
 WHITE="\033[1;37m"		# BOLD
-YELLOW="\033[1;33m"		# Warning
+yellow="\033[1;33m"		# Warning
 PADDING="  "
 DPADDING="\t\t"
 
@@ -48,11 +48,11 @@ DPADDING="\t\t"
 #### Other Colors / Status Code
 
 LGRAY="\033[0;37m"		# Light Gray
-LRED="\033[1;31m"		# Light Red
-LGREEN="\033[1;32m"		# Light GREEN
+red="\033[1;31m"		# Light Red
+green="\033[1;32m"		# Light GREEN
 LBLUE="\033[1;34m"		# Light Blue
 LPURPLE="\033[1;35m"	# Light Purple
-LCYAN="\033[1;36m"		# Light Cyan
+ltcyan="\033[1;36m"		# Light Cyan
 SORANGE="\033[0;33m"	# Standar Orange
 SBLUE="\033[0;34m"		# Standar Blue
 SPURPLE="\033[0;35m"	# Standar Purple      
